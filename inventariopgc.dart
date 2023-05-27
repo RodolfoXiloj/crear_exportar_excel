08cf5a3113c8545ba52afb4f237669f191202f47
Rodolfo <rodolfoxiloj@hotmail.com>
2023-05-27 16:29:48 -0600

    delete default sheet

--- inventariopgc.dart
+++ inventariopgc.dart
@@ -187,10 +187,11 @@ class _InventarioPGCState extends State<InventarioPGC> {
   Future<void> generarYGuardarExcel() async {
     // Crea una instancia de la clase Excel
     var excel = Excel.createExcel();
+    
+    excel.delete('Sheet1');
 
     // Crea una hoja en el archivo de Excel
-    var hoja = excel['Sheet1'];
-    hoja.name = 'Inventario';
+    var hoja = excel['Inventario'];
 
     // Agrega datos a la hoja de Excel
     hoja.appendRow(['Caja', 'Codigo', 'Cantidad']);
